234ae305eef59fc537180aba2c4a5ebe256d6772
Antti Partanen <aehparta@iki.fi>
2018-04-12 13:19:32 +0300

    sms balance check

--- Message.php
+++ Message.php
@@ -1,88 +1,87 @@
 <?php
 
 namespace Messaging;
+
 use kernel;
 
 class Message extends \Core\Module
 {
-	protected $options = array();
+    protected $options = array();
 
-	public function __construct()
-	{
-		parent::__construct();
-		$options = $this->getModuleValue('options');
-		if (is_array($options))
-		{
-			$this->options = $options;
-		}
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $options = $this->getModuleValue('options');
+        if (is_array($options)) {
+            $this->options = $options;
+        }
+    }
 
-	public function send(string $handler_type_or_class, string $subject, string $content, array $recipients)
-	{
-		if (empty($recipients))
-		{
-			kernel::log(LOG_NOTICE, 'no recipients for sending message, handler: ' . $handler_type_or_class . ', subject: ' . $subject);
-			return false;
-		}
-		$handler = null;
-		try
-		{
-			$handler_class = $handler_type_or_class[0] === '\\' ? $handler_type_or_class : '\\Messaging\\' . $handler_type_or_class . '\\Message';
-			$handler       = new $handler_class($this);
-		}
-		catch (Exception $e)
-		{
-			/* log error */
-			kernel::log(LOG_ERR, 'message handler class not found: ' . $handler);
-			return false;
-		}
-		if (!$handler->send($subject, $content, $recipients))
-		{
-			kernel::log(LOG_ERR, 'failed to send message, handler: ' . $handler_type_or_class . ', subject: ' . $subject);
-			return false;
-		}
-		return true;
-	}
+    public function send(string $handler_type_or_class, string $subject, string $content, array $recipients)
+    {
+        if (empty($recipients)) {
+            kernel::log(LOG_NOTICE, 'no recipients for sending message, handler: ' . $handler_type_or_class . ', subject: ' . $subject);
+            return false;
+        }
+        $hander = $this->getHandlerClass($handler_type_or_class);
+        if (!$handler->send($subject, $content, $recipients)) {
+            kernel::log(LOG_ERR, 'failed to send message, handler: ' . $handler_type_or_class . ', subject: ' . $subject);
+            return false;
+        }
+        return true;
+    }
 
-	public function setOption(string $handler, string $key, string $value)
-	{
-		if (!isset($this->options[$handler]))
-		{
-			$this->options[$handler] = array();
-		}
-		$this->options[$handler][$key] = $value;
-	}
+    public function setOption(string $handler, string $key, string $value)
+    {
+        if (!isset($this->options[$handler])) {
+            $this->options[$handler] = array();
+        }
+        $this->options[$handler][$key] = $value;
+    }
 
-	public function getOption(string $handler, string $key, $no_global = false)
-	{
-		if (isset($this->options[$handler][$key]))
-		{
-			return $this->options[$handler][$key];
-		}
-		else if (!$no_global && isset($this->options['global'][$key]))
-		{
-			return $this->options['global'][$key];
-		}
-		return null;
-	}
+    public function getOption(string $handler, string $key, $no_global = false)
+    {
+        if (isset($this->options[$handler][$key])) {
+            return $this->options[$handler][$key];
+        } else if (!$no_global && isset($this->options['global'][$key])) {
+            return $this->options['global'][$key];
+        }
+        return null;
+    }
 
-	public function setGlobalOption($key, $value)
-	{
-		$this->setOption('global', $key, $value);
-	}
+    public function setGlobalOption($key, $value)
+    {
+        $this->setOption('global', $key, $value);
+    }
 
-	public function getGlobalOption($key)
-	{
-		return $this->getOption('global', $key);
-	}
+    public function getGlobalOption($key)
+    {
+        return $this->getOption('global', $key);
+    }
 
-	public function setTypeOption($key, $value)
-	{
-		$this->setOption($this->getType(), $key, $value);
-	}
+    public function setTypeOption($key, $value)
+    {
+        $this->setOption($this->getType(), $key, $value);
+    }
 
-	public function getTypeOption($key, $no_global = false)
-	{
-		return $this->getOption($this->getType(), $key, $no_global);
-	}
+    public function getTypeOption($key, $no_global = false)
+    {
+        return $this->getOption($this->getType(), $key, $no_global);
+    }
+
+    private function getHandlerClass($handler_type_or_class)
+    {
+        $handler = null;
+        try
+        {
+            $handler_class = $handler_type_or_class[0] === '\\' ? $handler_type_or_class : '\\Messaging\\' . $handler_type_or_class . '\\Message';
+            $handler       = new $handler_class($this);
+        } catch (Exception $e) {
+            /* log error */
+            kernel::log(LOG_ERR, 'message handler class not found: ' . $handler);
+            return null;
+        }
+        return $handler;
+    }
+
 }
--- sms/Message.php
+++ sms/Message.php
@@ -1,95 +1,130 @@
 <?php
 
 namespace Messaging\sms;
+
 use kernel;
 
 class Message extends \Messaging\MessageHandler
 {
-	public function getType()
-	{
-		return 'sms';
-	}
+    public function getType()
+    {
+        return 'sms';
+    }
 
-	public function send(string $subject, string $content, array $recipients)
-	{
-		$gateway = $this->getModuleValue('gateway');
-		if (!$gateway)
-		{
-			kernel::log(LOG_ERR, 'missing sms gateway');
-			return false;
-		}
-		switch ($gateway)
-		{
-		case 'gatewayapi.com':
-			return $this->sendGatewayapiCom($subject, $content, $recipients);
-		default:
-			kernel::log(LOG_ERR, 'invalid sms gateway: ' . $gateway);
-			break;
-		}
-		return false;
-	}
+    public function send(string $subject, string $content, array $recipients)
+    {
+        $gateway = $this->getModuleValue('gateway');
+        if (!$gateway) {
+            kernel::log(LOG_ERR, 'missing sms gateway');
+            return false;
+        }
+        switch ($gateway) {
+            case 'gatewayapi.com':
+                return $this->sendGatewayapiCom($subject, $content, $recipients);
+            default:
+                kernel::log(LOG_ERR, 'invalid sms gateway: ' . $gateway);
+                break;
+        }
+        return false;
+    }
 
-	private function sendGatewayapiCom($subject, $content, $recipients)
-	{
-		$token   = $this->getModuleValue('token');
-		$username = $this->getModuleValue('username');
-		$password = $this->getModuleValue('password');
-		if (!$token && (!$username || !$password))
-		{
-			kernel::log(LOG_ERR, 'gatewayapi.com needs token or username and password');
-			return false;
-		}
-		$data = array(
-			'message'    => (empty($subject) ? '' : $subject . "\n") . $content,
-			'recipients' => array(),
-		);
-		if ($this->getOption('sender'))
-		{
-			/* gatewayapi.com accepts at most 11 sender characters */
-			$data['sender'] = substr($this->getOption('sender'), 0, 11);
-		}
-		foreach ($recipients as $recipient)
-		{
-			$data['recipients'][] = array('msisdn' => $recipient);
-		}
+    public function checkBalance()
+    {
+        $gateway = $this->getModuleValue('gateway');
+        if (!$gateway) {
+            kernel::log(LOG_ERR, 'missing sms gateway');
+            return false;
+        }
+        switch ($gateway) {
+            case 'gatewayapi.com':
+                return $this->checkGatewayapiComBalance();
+            default:
+                kernel::log(LOG_ERR, 'invalid sms gateway: ' . $gateway);
+                break;
+        }
+        return false;
+    }
 
-		$headers = array(
-			'Accept: application/json, text/javascript',
-			'Content-Type: application/json',
-		);
+    private function sendGatewayapiCom($subject, $content, $recipients)
+    {
+        $token    = $this->getModuleValue('token');
+        $username = $this->getModuleValue('username');
+        $password = $this->getModuleValue('password');
+        if (!$token && (!$username || !$password)) {
+            kernel::log(LOG_ERR, 'gatewayapi.com needs token or username and password');
+            return false;
+        }
+        $data = array(
+            'message'    => (empty($subject) ? '' : $subject . "\n") . $content,
+            'recipients' => array(),
+        );
+        if ($this->getOption('sender')) {
+            /* gatewayapi.com accepts at most 11 sender characters */
+            $data['sender'] = substr($this->getOption('sender'), 0, 11);
+        }
+        foreach ($recipients as $recipient) {
+            $data['recipients'][] = array('msisdn' => $recipient);
+        }
 
-		/* url is hardcoded on purpose mostly to avoid accidents! */
-		$url  = 'https://gatewayapi.com/rest/mtsms' . ($token ? '?token=' . $token : '');
-		$data = $this->post($url, json_encode($data), $username, $password, $headers);
-		$data = @json_decode($data, true);
+        $headers = array(
+            'Accept: application/json, text/javascript',
+            'Content-Type: application/json',
+        );
 
-		return $data === false ? false : true;
-	}
+        /* url is hardcoded on purpose mostly to avoid accidents! */
+        $url  = 'https://gatewayapi.com/rest/mtsms' . ($token ? '?token=' . $token : '');
+        $data = $this->request($url, json_encode($data), $username, $password, $headers);
+        $data = @json_decode($data, true);
 
-	private function post($url, $data, $username, $password, $headers = array())
-	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		if ($username && $password)
-		{
-			curl_setopt($ch, CURLOPT_USERPWD, $username . ':' . $password);
-		}
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        return $data === false ? false : true;
+    }
 
-		$data = curl_exec($ch);
-		if (curl_error($ch))
-		{
-			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			kernel::log(LOG_ERR, 'message sms send POST request failed, http code: ' . $code . ', reason: ' . curl_error($ch));
-			curl_close($ch);
-			return false;
-		}
-		curl_close($ch);
+    private function checkGatewayapiComBalance()
+    {
+        $token    = $this->getModuleValue('token');
+        $username = $this->getModuleValue('username');
+        $password = $this->getModuleValue('password');
+        if (!$token && (!$username || !$password)) {
+            kernel::log(LOG_ERR, 'gatewayapi.com needs token or username and password');
+            return false;
+        }
+        $headers = array(
+            'Accept: application/json, text/javascript',
+            'Content-Type: application/json',
+        );
 
-		return $data;
-	}
+        /* url is hardcoded on purpose mostly to avoid accidents! */
+        $url  = 'https://gatewayapi.com/rest/me' . ($token ? '?token=' . $token : '');
+        $data = $this->request($url, null, $username, $password, $headers);
+        $data = @json_decode($data, true);
+
+        return $data === false ? false : $data;
+    }
+
+    private function request($url, $data, $username, $password, $headers = array())
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($username && $password) {
+            curl_setopt($ch, CURLOPT_USERPWD, $username . ':' . $password);
+        }
+        if ($data) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $data = curl_exec($ch);
+        if (curl_error($ch)) {
+            $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            kernel::log(LOG_ERR, 'message request failed, http code: ' . $code . ', reason: ' . curl_error($ch));
+            curl_close($ch);
+            return false;
+        }
+        curl_close($ch);
+
+        return $data;
+    }
 }
